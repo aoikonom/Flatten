a664af0908ffb8ff705b379ce8d3ee1efe4ba787
Andreas Oikonomou <aoikonom@yahoo.com>
2017-06-22 14:18:00 +0300

    Add files via upload

--- Flatten.js
+++ Flatten.js
@@ -1,4 +1,4 @@
-function flatten(arr) {
+function flattenRecursive(arr) {
     let result = [];
     for (let item of arr)        
         if (item instanceof Array) 
@@ -8,10 +8,32 @@ function flatten(arr) {
     return result;
 }
 
+function flatten(arr) {
+    let result = arr.toString().split(",");
+    for (let i = 0; i < result.length; i++)
+        result[i] = parseInt(result[i]);
+    return result;
+}
+
+function simpleArrayEqual(a, b) {
+    if (a.length != b.length)
+        return false;
+    for (let i = 0; i < a.length; i++)
+        if (a[i] !== b[i])
+            return false;
+    return true;
+}
+
 
 function testFlatten() {
-    console.log(flatten([3, [3, 2]]));
-    console.log(flatten([3,5, [11, 15, 3, [2, 4]], 12, [2, [[2, 4], 11], 10], [1,2]]));
+    let a1 = [3, [3,2]];
+    let a2 = [3, 3,  2];
+    let b1 = [3,5, [11, 15, 3, [2, 4]], 12, [2, [[2, 4], 11], 10], [1,2]];
+    let b2 = [3, 5, 11, 15, 3, 2, 4, 12, 2, 2, 4, 11, 10, 1, 2];
+    console.assert(simpleArrayEqual(flatten(a1),a2), "Error in flatten");
+    console.assert(simpleArrayEqual(flatten(b1), b2), "Error in flatten");
+    console.assert(simpleArrayEqual(flattenRecursive(a1),a2), "Error in flatten");
+    console.assert(simpleArrayEqual(flattenRecursive(b1), b2), "Error in flatten");
 }
 
 testFlatten();
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Flatten
-Flatten Exercise 
+# Project Title
+
+Flatten array
+
+## Getting Started
+Write some code, that will flatten an array of arbitrarily nested arrays of integers into a flat array of integers. e.g.
+```
+[[1,2,[3]],4] -> [1,2,3,4]. 
+```
+
+Two functions are give flatten and flattenRecursive
+
+
+## Running the tests
+
+testFlattent() performs some tests
+
+### Break down into end to end tests
+
+
+
+```
+ flatten([3,5, [11, 15, 3, [2, 4]], 12, [2, [[2, 4], 11], 10], [1,2]]) = [3, 5, 11, 15, 3, 2, 4, 12, 2, 2, 4, 11, 10, 1, 2]
+```
+
+
+## Built With
+
+Node.js
+
+
+## Authors
+
+* **Andreas Oikonomou** 
+
